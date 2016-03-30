e1e0d850559ea89cbc2e068e0b26d9f4ceffa717
jordan byrd <jabyrd3@gmail.com>
2016-03-30 15:48:42 -0400

    cleanup, readme, todo

---
+++ readme.md
@@ -0,0 +1,57 @@
+# Stop sweating the tech screening process
+It's too easy to project bias into the tech hiring process. Winnow is small commandline utility that allows you to blindly send sample codetests and check the validity of the applicants answer using only their email. It also keeps track of who you've sent tests to in a flat SQL file that is exportable / readable by anything that can manage sqlite.
+
+# Installation
+You'll need to edit config.js.sample. All fields are mandatory. You'll have to register a personal API access token with github in order to use it. Make sure to give that token permissions to modify/create repos in the github UI.
+
+```
+git clone https://github.com/jabyrd3/winnow
+cd winnow
+npm install
+node winnow
+```
+
+# Status
+This is a super pre-alpha. Use at your own risk. I'll try to get a sample code test up in the next couple days to show how to use this with your own tests.
+
+# Tech
+Vorpal is the main driver of the current interface. Internally it uses a single-table sqlite database to keep track of your issued tests. Git integration is a mix of https calls and Nodegit, tests are run with jsdom. Obfuscation is a simple uglify-js call.
+
+# Data
+To export your data simply navigate to the winnow directory on your machine and copy out winnow.data. It's a non-encrypted sqlite database built via sqlite3.
+
+# Usage
+```
+node winnow
+```
+The winnow command opens an interactive shell-like environment via vorpal. From there, you can send code tests, view your sent tests, and check results from individual candidates.
+
+# Available commands
+send <email> <tag> - clones a repo from config.js, removes any git data, and pushes it up to a new repo
+using the credentials from config.js, sends an email to the <email> arg with a link to the codetest, and marks down the tag/email/github url of the testee in db.
+
+list - lists all the testees in the db. tag: email @ https://github.com/yoururl/generated-test
+
+check <tag> - downloads the target repository, obfuscates any files you want hidden from the end user, runs the users code, and fires a simple equality test (for now), against 2 objects to test if the testee solved the problem.
+
+clean:tmp - util command removes the tmp directory in case something breaks.
+
+clean:db - clears entire testee db. this is none recoverable.
+
+# Todo
+- add more complicated testing capabilites.
+- delete single items by tagname
+- search by email
+- search by tagname
+- delete via regex/glob
+- save test results
+- check all tests?
+- colors
+- tab complete
+- package for npm
+- table view
+- table interactivity?
+- named code tests
+- multiple code tests
+- fancier config?
+- interactive setup
--- winnow.js
+++ winnow.js
@@ -12,6 +12,7 @@ var fs = require('fs');
 var canvas = require('canvas');
 var jsdom = require('jsdom');
 var _ = require('lodash');
+var crypto = require('crypto');
 db.serialize(function() {
     // quick and dirty db setup
     db.run("CREATE TABLE IF NOT EXISTS applicants (email TEXT, tag TEXT, url TEXT); ", [], function(err, rows) {});
@@ -27,7 +28,7 @@ vorpal
                     fs.writeFileSync(path, obfuscate.code, 'utf8');
                 });
                 exec('cd ./tmp; git init');
-                var hash = Math.random();
+                var hash = crypto.createHash('md5').update(`${args.tag} ${args.email}`).digest('hex');
                 //post new repo to github
                 request.post({
                         url: `https://${config.privToken}:x-oauth-basic@api.github.com/user/repos`,
@@ -35,7 +36,7 @@ vorpal
                             'User-Agent': 'winnow-code-test'
                         },
                         json: {
-                            "name": hash,
+                            "name": `${args.tag}-${hash}`,
                             "description": "This is a codetest from winnow",
                             "private": false,
                             "has_issues": true,
@@ -52,14 +53,14 @@ vorpal
                         }
                         rimraf.sync('rm tmp/.git');
                         exec('cd tmp; git init; git add .; git commit -m \'init\'');
-                        exec(`cd tmp; git remote add origin ${config.gitProfileUrl}/${hash}.git`);
+                        exec(`cd tmp; git remote add origin ${config.gitProfileUrl}/${args.tag}-${hash}.git`);
                         exec(`cd tmp; git push -u origin master`);
                         rimraf.sync('tmp');
 
                         db.run("INSERT INTO applicants (email, tag, url) VALUES ($email, $tag, $url)", {
                             $email: args.email,
                             $tag: args.tag,
-                            $url: `${config.gitProfileUrl}/${hash}`
+                            $url: `${config.gitProfileUrl}/${args.tag}-${hash}`
                         }, function(err) {
                             if (err) {
                                 console.log(error);
@@ -92,6 +93,18 @@ vorpal
         })
     });
 vorpal
+    .command('clean:tmp', 'removes tmp dir')
+    .action(function(args, callback) {
+        rimraf.sync('tmp');
+        return callback();
+    });
+vorpal
+    .command('clean:db', 'clean db')
+    .action(function(args, callback) {
+        rimraf.sync('winnow.data');
+        return callback();
+    });
+vorpal
     .command('check <tag>', 'checks pr against branch, runs pr and compares results.')
     .action(function(args, callback) {
         db.each(`SELECT * FROM applicants WHERE tag="${args.tag}"`, function(err, row) {
@@ -100,9 +113,7 @@ vorpal
             }
             rimraf.sync('tmp');
             Git.Clone(row.url, "./tmp").then(function() {
-                // console.log(row);
                 // here is where we run the tests
-                // cleanup
                 var html = fs.readFileSync(`${__dirname}/tmp/${config.indexPath}`, 'utf8');
 
                 // debug
