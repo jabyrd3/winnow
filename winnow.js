5a8b4132773d881797bb612b68733d4811916e1c
jordan byrd <jabyrd3@gmail.com>
2016-03-30 00:08:02 -0400

    mvp

--- winnow.js
+++ winnow.js
@@ -8,18 +8,21 @@ var rimraf = require('rimraf');
 var exec = require('sync-exec');
 var request = require('request');
 var config = require('./config.js');
+var fs = require('fs');
 db.serialize(function() {
     // quick and dirty db setup
-    db.run("CREATE TABLE IF NOT EXISTS profiles (email text, tag TEXT, url TEXT); ", [], function(err, rows) {});
+    db.run("CREATE TABLE IF NOT EXISTS applicants (email TEXT, tag TEXT, url TEXT); ", [], function(err, rows) {});
 });
 vorpal
-    .command('send', 'create hashed branch of config, sends code test to email, tags email with name and saves all data.')
+    .command('send <email> [tag]', 'create hashed branch of config, sends code test to email, tags email with name and saves all data.')
     .action(function(args, callback) {
         Git.Clone(config.testSource, "./tmp")
             .then(function(repo) {
-                console.log('repo');
-                console.log('repo', repo);
                 rimraf.sync('tmp/.git');
+                config.obfuscate.forEach(function(path) {
+                    var obfuscate = UglifyJS.minify(path);
+                    fs.writeFileSync(path, obfuscate.code, 'utf8');
+                });
                 exec('cd ./tmp; git init');
                 var hash = Math.random();
                 //post new repo to github
@@ -46,32 +49,64 @@ vorpal
                         }
                         rimraf.sync('rm tmp/.git');
                         exec('cd tmp; git init; git add .; git commit -m \'init\'');
-                        exec(`cd tmp; git remote add origin https://github.com/jabyrd3/${hash}.git`);
+                        exec(`cd tmp; git remote add origin ${config.gitProfileUrl}/${hash}.git`);
                         exec(`cd tmp; git push -u origin master`);
                         rimraf.sync('tmp');
-                        return callback();
+
+                        db.run("INSERT INTO applicants (email, tag, url) VALUES ($email, $tag, $url)", {
+                            $email: args.email,
+                            $tag: args.tag,
+                            $url: `${config.gitProfileUrl}/${hash}`
+                        }, function(err) {
+                            if (err) {
+                                console.log(error);
+                                return callback();
+                            }
+                            console.log('success');
+                            return callback();
+                        });
                     });
             })
             .catch(function(err) {
                 console.log('there was an oops, regrettable', err);
                 rimraf.sync('tmp');
-                console.log('error: ', err);
                 return callback();
             });
     });
 
-// vorpal
-//     .command('list', 'lists emails and tags')
-//     .action(function(args, callback) {
-//         this.log('bar');
-//         callback();
-//     });
-// vorpal
-//     .command('check', 'checks pr against branch, runs pr and compares results.')
-//     .action(function(args, callback) {
-//         this.log('bar');
-//         callback();
-//     });
+vorpal
+    .command('list', 'lists emails and tags')
+    .action(function(args, callback) {
+        db.all('SELECT * FROM applicants', function(err, rows) {
+            if (err) {
+                console.log(err);
+                return callback();
+            }
+            rows.forEach(function(row) {
+                console.log(`${row.tag}: ${row.email} @ ${row.url}`);
+            })
+            return callback();
+        })
+    });
+vorpal
+    .command('check', 'checks pr against branch, runs pr and compares results.')
+    .action(function(args, callback) {
+        db.each('SELECT * FROM applicants WHERE tag="foo"', function(err, row) {
+            if (err) {
+                console.log(err);
+            }
+            Git.Clone(row.url, "./tmp").then(function() {
+                console.log(row);
+                // here is where we run the tests
+                // cleanup
+                // rimraf.sync('tmp');
+                return callback();
+            }).catch(function(err) {
+                console.log('i regret to inform you that there has been a catastrophic oops', err);
+                rimraf.sync('tmp');
+            });
+        })
+    });
 vorpal
     .delimiter('winnow$')
     .show();
